9396dc5ec22390d9332dd11f98d9d85b65e0b14d
mattdood <mawimberly@csumb.edu>
2022-01-28 19:21:03 -0800

    Conftest and pytest added, creates test db on startup and destroys on teardown

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+import shutil
+
+from src.ein.database import Database
+
+TEST_DB = "test.db"
+
+@pytest.fixture()
+def db_setup():
+    """Creates a workspace DB on startup."""
+    return Database(db_path=TEST_DB)
+
+
+@pytest.fixture(autouse=True)
+def cleanup_run():
+    """Removes workspaces on teardown."""
+    yield
+    if os.path.exists(TEST_DB):
+        os.remove(TEST_DB)
+
