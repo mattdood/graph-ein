fe02cbd1bd1c79724158f6acf95875159fe38c39
mattdood <mawimberly@csumb.edu>
2022-01-28 20:36:13 -0800

    Python obj of the graph DB records added for node and edge

--- src/ein/edge.py
+++ src/ein/edge.py
@@ -1,9 +1,21 @@
+from typing import Dict
 
 
 class Edge:
+    """Edge object from the SQLite db."""
 
-    def __init__(self):
-        pass
+    def __init__(self, source_id: str, target_id: str, properties: Dict=None):
+        """Representation of schema from {{schema_name}}_nodes."""
+        self._source_id = source_id
+        self._target_id = target_id
+        self._properties = properties
 
+    def get_source_id(self):
+        return self._source_id
 
+    def get_target_id(self):
+        return self._target_id
+
+    def get_properties(self):
+        return self._properties
 
--- src/ein/node.py
+++ src/ein/node.py
@@ -1,7 +1,17 @@
+from typing import Dict
 
 
 class Node:
+    """Node object from the SQLite db."""
 
-    def __init__(self):
-        pass
+    def __init__(self, id: str, body: Dict):
+        """Representation of schema from {{schema_name}}_nodes."""
+        self._id = id
+        self._body = body
+
+    def get_id(self):
+        return self._id
+
+    def get_body(self):
+        return self._body
 
