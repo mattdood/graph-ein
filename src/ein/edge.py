72d203e3163c7c9deda65fff9b669e4e769581a7
mattdood <mawimberly@csumb.edu>
2022-05-13 20:50:50 -0700

    Edge: Data representation with comparison created

--- src/ein/edge.py
+++ src/ein/edge.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from .node import Node
 
@@ -6,18 +6,21 @@ from .node import Node
 class Edge:
     """Edge object from the SQLite db."""
 
-    def __init__(self, source_id: Node, target_id: Node, properties: Dict=None):
+    def __init__(self,
+                 schema_name: str,
+                 source_id: Node,
+                 target_id: Node,
+                 properties: Optional[Dict] = None) -> None:
         """Representation of 'edge' from {{schema_name}}_edges."""
+        self.schema_name = schema_name
         self.source_id = source_id
         self.target_id = target_id
         self.properties = properties
 
-    def update_source_id(self, source_id: Node) -> None:
-        self.source_id = source_id
-
-    def update_target_id(self, target_id: Node) -> None:
-        self.target_id = target_id
-
-    def update_properties(self, properties: Dict) -> None:
-        self.properties = properties
+    def __eq__(self, source_id: str, target_id: str) -> bool:
+        return (
+            self.source_id == source_id
+            and
+            self.target_id == target_id
+        )
 
