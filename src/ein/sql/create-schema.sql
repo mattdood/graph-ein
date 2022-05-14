d27cb549b4cf7e5562142770abe0aebd0df2e2a7
mattdood <mawimberly@csumb.edu>
2022-05-13 20:51:13 -0700

    Database: Schema create now properly enforces foreign keys

--- src/ein/sql/create-schema.sql
+++ src/ein/sql/create-schema.sql
@@ -1,3 +1,7 @@
+-- enable foreign key enforcement
+PRAGMA foreign_keys = ON;
+
+-- create nodes table with index
 CREATE TABLE IF NOT EXISTS {{schema_name}}_nodes (
     id TEXT NOT NULL PRIMARY KEY,
     body JSON
@@ -5,13 +9,14 @@ CREATE TABLE IF NOT EXISTS {{schema_name}}_nodes (
 
 CREATE INDEX IF NOT EXISTS idx_id ON {{schema_name}}_nodes(id);
 
+-- create edges table with weighted properties
 CREATE TABLE IF NOT EXISTS {{schema_name}}_edges (
     source TEXT,
     target TEXT,
     properties TEXT,
     UNIQUE(source, target, properties) ON CONFLICT REPLACE,
-    FOREIGN KEY(source) REFERENCES nodes(id),
-    FOREIGN KEY(target) REFERENCES nodes(id)
+    FOREIGN KEY(source) REFERENCES nodes(id) ON DELETE CASCADE,
+    FOREIGN KEY(target) REFERENCES nodes(id) ON DELETE CASCADE
 );
 
 CREATE INDEX IF NOT EXISTS idx_source ON {{schema_name}}_edges(source);
