c2cbb0d8e9b9347103e20204e66428b701d20742
mattdood <mawimberly@csumb.edu>
2022-05-30 17:29:47 -0700

    PYPI: POC of distribution setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graph-ein",
+    version="0.0.1",
+    author="Matthew Wimberly",
+    author_email="matthew.wimb@gmail.com",
+    description="A graph database implemented in SQLite.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattdood/graph-ein",
+    project_urls={
+        "Bug Tracker": "https://github.com/mattdood/graph-ein/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
