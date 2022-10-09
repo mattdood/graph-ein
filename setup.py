4f244d170f5b59dda3168a2c59476d55b3aeef1c
mattdood <mawimberly@csumb.edu>
2022-10-08 17:35:04 -0700

    Upversion: v0.0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ein-graph",
-    version="0.0.11",
+    version="0.0.12",
     author="Matthew Wimberly",
     author_email="matthew.wimb@gmail.com",
     description="A graph database implemented in SQLite.",
